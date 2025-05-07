7dde34b2edf8a9884cbe52389a313f007d0dd723
SP4CEVI <95271930+SP4CEVI@users.noreply.github.com>
2025-05-07 17:08:52 +0300

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,65 +1,27 @@
-export interface Product {
-    id: string;
-    title: string;
-    category: string;
-    description: string;
-    price: number | null;
-    image: string;
+
+export type PaymentMethod = 'cash' | 'card';
+
+export type ProductType = 'хард-скил' | 'софт-скил' | 'дополнительное' | 'кнопка' | string;
+
+export interface IProduct {
+	id: string;
+	title: string;
+	image: string;
+	price: number;
+	description: string;
+	category: ProductType;
 }
 
-type ApiProductResponse = Product;
-
-type PaymentType = 'online' | 'cash';
-
-interface Client {
-    paymentMethod: string;
-    deliveryAddress: string;
-    emailAddress: string;
-    phoneNumber: string;
+export interface IOrder {
+	email: string;
+	phone: string;
+	address: string;
+	payment: PaymentMethod | null;
+	total: number;
+	items: string[];
 }
 
-interface Order {
-    itemIds: string[];
-    totalAmount: number;
-    paymentMethod: PaymentType;
-    deliveryAddress: string;
-    emailAddress: string; 
-    phoneNumber: string;
-}
-
-type OrderItem = Pick<Product, 'id' | 'title' | 'price'>;
-type GalleryItem = Omit<Product, 'description'>;
-type DeliveryOptions = Pick<Client, 'paymentMethod' | 'deliveryAddress'>;
-type ContactDetails = Pick<Client, 'emailAddress' | 'phoneNumber'>;
-
-type OrderDetails = DeliveryOptions & ContactDetails;
-
-interface OrderConfirmation {
-    orderId: string;
-    totalAmount: number;
-}
-
-type ApiOrderConfirmationResponse = OrderConfirmation;
-
-type SuccessResponse = Pick<OrderConfirmation, 'totalAmount'>;
-
-type FormValidationErrors<T> = Partial<Record<keyof T, string | null>>;
-
-interface Basket {
-    products: Product[];
-    totalPrice: number;
-    addProduct(product: Product): void;
-    removeProduct(productId: string): void;
-    clear(): void;
-    updateTotalPrice(priceChange: number): void;
-}
-
-interface Card {
-    product: Product;
-    render(): string;
-}
-
-interface Modal {
-    open(content: string): void;
-    close(): void;
+export interface ITotal {
+	id: string;
+	total: number;
 }
